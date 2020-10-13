04e25934ec5cdbd68a0dc374e36412df783b725e
EKastensmidt <64220094+EKastensmidt@users.noreply.github.com>
2020-10-13 05:58:06 -0300

    Finish line

---
+++ Assets/Scripts/Finish.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Finish : MonoBehaviour
+{
+    string currentScene;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        currentScene = SceneManager.GetActiveScene().name;
+        if (other.gameObject.name == "Player")
+        {
+            if (currentScene == "SampleScene")
+            {
+                SceneManager.LoadScene("Level1");
+            }
+        }   
+    }
+}
