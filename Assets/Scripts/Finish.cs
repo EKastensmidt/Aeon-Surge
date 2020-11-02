15988d3963582d475881c61af926bd0688e84531
EKastensmidt <64220094+EKastensmidt@users.noreply.github.com>
2020-11-02 20:53:34 -0300

    Level3

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -23,9 +23,19 @@ public class Finish : MonoBehaviour
             }
             else if (currentScene == "Level2")
             {
+                SceneManager.LoadScene("Level3");
+            }
+            else if (currentScene == "Level3")
+            {
+                //SceneManager.LoadScene("Level4");
                 SceneManager.LoadScene("Menu");
                 Cursor.lockState = CursorLockMode.None;
-
+            }
+            else if (currentScene == "Level4")
+            {
+                //SceneManager.LoadScene("Level5");
+                SceneManager.LoadScene("Menu");
+                Cursor.lockState = CursorLockMode.None;
             }
         }   
     }
---
+++ Assets/Scripts/New Folder/Lvl3Elevator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lvl3Elevator : MonoBehaviour
+{
+    public GameObject anim;
+    bool isActivated = true;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isActivated == true)
+        {
+            Debug.Log("MOVE!!!");
+            anim.GetComponent<Animation>().CrossFade("Elevator");
+            isActivated = false;
+        }
+    }
+}
