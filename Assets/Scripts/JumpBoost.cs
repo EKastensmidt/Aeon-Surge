98062a759691d964df46eae88dad0bd6726d0ac1
EKastensmidt <64220094+EKastensmidt@users.noreply.github.com>
2020-11-03 19:43:01 -0300

    avance

---
+++ Assets/Scripts/JumpBoost.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpBoost : MonoBehaviour
+{
+    bool isBoosted = false;
+    public float multiplier;
+    public float duration = 4f;
+    PlayerMovement stats;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            StartCoroutine(Pickup(other));
+        }
+    }
+
+    IEnumerator Pickup(Collider player)
+    {
+        stats = player.GetComponent<PlayerMovement>();
+        if (isBoosted == false)
+        {
+            isBoosted = true;
+            SoundManagerScript.PlaySound("PowerUp");
+            stats.jumpHeight *= multiplier;
+            yield return new WaitForSeconds(duration);
+            isBoosted = false;
+            SoundManagerScript.PlaySound("PowerDown");
+            stats.jumpHeight /= multiplier;
+        }
+    }
+}
