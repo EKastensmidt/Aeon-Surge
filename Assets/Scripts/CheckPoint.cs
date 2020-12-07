c6cbf7186fcafa4abba3410cc5d2cdd251a0bf17
EKastensmidt <64220094+EKastensmidt@users.noreply.github.com>
2020-12-07 19:18:39 -0300

    asd

---
+++ Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    public Transform checkPoint;
+    GameObject player;
+    private void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            player.transform.position = checkPoint.position;
+            player.transform.rotation = checkPoint.rotation;
+        }
+    }
+}
