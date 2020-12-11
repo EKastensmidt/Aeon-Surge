68032367167c0ba33bb86ef17916f0e00e737d8f
EKastensmidt <64220094+EKastensmidt@users.noreply.github.com>
2020-12-11 00:23:03 -0300

    sad

---
+++ Assets/Scripts/BossFight/BFButtons.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BFButtons : MonoBehaviour
+{
+    public static bool isActivated = false;
+    public GameObject anim;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(isActivated == true)
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.blue;
+            BFButtons.isActivated = false;
+            anim.GetComponent<Animation>().CrossFade("BFActivate");
+        }
+        else
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.green;
+            BFButtons.isActivated = true;
+            anim.GetComponent<Animation>().CrossFade("BFDeactivate");
+        }
+    }
+}
